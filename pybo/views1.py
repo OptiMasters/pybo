4dbc2dab1f51af6ff26682705b5d1c5fee2f8435
곽태온 <taeonomics@gmail.com>
2024-03-21 17:44:50 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/views1.py
@@ -0,0 +1,10 @@
+# Create your views here.
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question, Answer, Comment
+from django.utils import timezone
+from .forms import QuestionForm, AnswerForm, CommentForm
+
+
